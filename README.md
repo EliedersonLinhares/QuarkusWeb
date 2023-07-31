b0667625a6e357fa627404830b9f99b4f717cbfb
EliedersonLinhares <eliederson250@gmail.com>
2023-07-31 11:49:15 -0300

    Crud operations created

---
+++ README.md
@@ -0,0 +1,23 @@
+# Desenvolvimetno Web com Quarkus
+
+Projeto básico de uso do Quarkus em java para estudo.
+
+- Java 17
+- Quarkus
+- PostgreSql
+- Swagger
+
+
+## Rodando a aplição em desensenvolvimento
+
+Crie um banco de dados no PostgreSQL com nome definido no Apllication.Properties
+
+Você pode rodar a aplição com atualização automática digitando no terminal:
+```shell script
+./mvnw compile quarkus:dev
+```
+
+> **_NOTA:_** Para acessar a UI do quarkus: http://localhost:8080/q/dev/.
+> 
+>  para acessar o Swager: http://localhost:8080/q/swagger/
+
---
+++ src/main/java/org/acme/user/UserController.java
@@ -0,0 +1,85 @@
+package org.acme.user;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@Path("/user")
+public class UserController {
+
+    @Inject
+    UserRepository userRepository;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllUsers(){
+        List<UserModel> users = userRepository.listAll();
+        return Response.ok(users).build();
+    }
+
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveUser(UserModel user){
+        userRepository.persist(user);
+        if(userRepository.isPersistent(user)){
+            return Response.created(URI.create("/user/" + user.id)).build();
+        }
+        else{
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUserById(@PathParam("id") Long id){
+        UserModel user = userRepository.findById(id);
+        return Response.ok(user).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateUser(@PathParam("id") Long id, UserModel user){
+        Optional<UserModel> optionalUserModel = userRepository.findByIdOptional(id);
+        if(optionalUserModel.isPresent()){
+            UserModel updateUser = optionalUserModel.get();
+            updateUser.setFirstName(user.getFirstName());
+            updateUser.setLastName(user.getLastName());
+            updateUser.setEmail(user.getEmail());
+            updateUser.setGender(user.getGender());
+            userRepository.persist(updateUser);
+            if(userRepository.isPersistent(updateUser)){
+                return  Response.ok().build();
+            }else{
+                return  Response.status((Response.Status.BAD_REQUEST)).build();
+            }
+        }
+       return  Response.status((Response.Status.BAD_REQUEST)).build();
+    }
+
+    @DELETE
+    @Path("/{id}/")
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteUser(@PathParam("id") Long id){
+       boolean isDeleted = userRepository.deleteById(id);
+       if(isDeleted){
+           return Response.noContent().build();
+       }
+       else{
+           return Response.status(Response.Status.BAD_REQUEST).build();
+       }
+    }
+
+}
