2eb2b1214757ead8874e089deab8233e71085f4c
EliedersonLinhares <eliederson250@gmail.com>
2023-08-09 00:01:38 -0300

    Stying the swagger-ui page

--- src/main/java/org/acme/user/UserController.java
+++ src/main/java/org/acme/user/UserController.java
@@ -8,7 +8,10 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import java.net.URI;
 import java.util.Map;
@@ -28,13 +31,23 @@ public class UserController {
     @GET
     @RolesAllowed({"admin","user"})
     @Path("/{id}")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Successfully retrieved"),
+            @APIResponse(responseCode = "404", description = "User not found")
+    })
+    @Operation( description = "Returns a user as per the id,role user only can return your own data, admin can return anyone")
     public Response getUserById(@PathParam("id") Long id){
 
         UserModel user = userService.getUserById(id);
         return Response.ok(user).build();
     }
-    @GET
+    @POST
     @Path("/login")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Successfully login"),
+            @APIResponse(responseCode = "404", description = "User not found or Credentials invalid"),
+    })
+    @Operation(description = "Login user and generate a JWT token and store on cookie")
     public Response loginByEmail(@RequestBody LoginDto loginDto){
       Map<String,Object> response =  userService.authenticate(loginDto);
 
@@ -42,17 +55,17 @@ public class UserController {
         return Response.ok(response.get("user")).header("Set-Cookie", "jwt="+response.get("token")+ ";Expires="+cookieExpires+"; HttpOnly=true ").build();
     }
 
-
-
     @GET
     @Path("/logout")
+    @Operation(description = "Logout invalidating the cookie")
     public Response logout(){
         return Response.ok().header("Set-Cookie", "jwt=;Expires=;").build();
     }
 
     @GET
-   @RolesAllowed("admin")
+    @RolesAllowed("admin")
     @Path("/userpaginated")
+    @Operation(description = "Return a list of users paginated, and with filters")
     public Response getAllUsersPaginated(
             @QueryParam("size")@DefaultValue("5") int size,
             @QueryParam("page") @DefaultValue("0") int page,
@@ -82,6 +95,7 @@ public class UserController {
     @POST
     @PermitAll
     @Transactional
+    @Operation(description = "Register a new user")
     public Response saveUser(@Valid @RequestBody UserDto user){
       userService.save2User(user);
      return Response.created(URI.create("/user/" + user.id())).build();
@@ -89,9 +103,10 @@ public class UserController {
     }
 
     @PUT
-    @RolesAllowed("user")
+    @RolesAllowed({"user","admin"})
     @Transactional
     @Path("/{id}")
+    @Operation(description = "Update basic data of the user, role user only can update your own data, admin can update any user data")
     public Response updateUser(@Valid @PathParam("id") Long id, @RequestBody UpdateUserDto userDto){
             userService.update2User(userDto,id);
             return  Response.ok().build();
@@ -101,6 +116,7 @@ public class UserController {
     @RolesAllowed("admin")
     @Transactional
     @Path("/roles/{id}")
+    @Operation(description = "Update user status role, can be made only by admin")
     public Response updateUserRole(@PathParam("id") Long id, @RequestBody UpdateUserRole userDto){
 
         userService.updateUserRole(userDto,id);
@@ -110,6 +126,7 @@ public class UserController {
     @RolesAllowed({"user","admin"})
     @Transactional
     @Path("/password/{id}")
+    @Operation(description = "Update user password, role user can update your own password")
     public Response updateUserPassword(@PathParam("id") Long id, @RequestBody UpdatePassword userDto){
 
         userService.updateUserPassword(userDto,id);
@@ -119,6 +136,7 @@ public class UserController {
     @DELETE
    @RolesAllowed("admin")
     @Path("/{id}")
+    @Operation(description = "Delete a user from system")
     public Response deleteUser(@PathParam("id") Long id){
        userService.deleteUser(id);
        return Response.noContent().build();
